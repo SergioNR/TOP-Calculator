f6b09e971b5ccc236d2e8a72e1dd5727bc483543
Sergio Navarro <s.navarroredondo@gmail.com>
2023-11-18 23:25:37 +0100

    ADD basic mathematical functions
    added logic for:
    - Add
    - Subtract
    - Multiply
    - Divide

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
+// add
+// subtract
+// multiply
+// divide
+
+const addNumber = (num1, num2) => {
+    return num1 + num2
+}
+
+const subtractNumber = (num1, num2) => {
+    return num1 - num2
+}
+
+const multuplytNumber = (num1, num2) => {
+    return num1 * num2
+}
+
+const divideNumber = (num1, num2) => {
+    return num1 / num2
+}
+
+addNumber(1,2)
+subtractNumber(2,1)
+multuplytNumber(3,4)
+divideNumber(4,2)
